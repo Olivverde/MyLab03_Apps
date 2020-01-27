1d765078cedb3e248c1b1f29db6375c33e7ffd84
Olivverde19270 <oliver.milian00@gmail.com>
2020-01-27 08:09:37 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mylab03_apps/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mylab03_apps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
